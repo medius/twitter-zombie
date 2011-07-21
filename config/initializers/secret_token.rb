e9cee61aff5bd063397dd93328a9e4c990aa759d
Puru <puruchoudhary@gmail.com>
2011-07-20 18:34:08 -0700

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetZombie::Application.config.secret_token = 'e864ee78416e9dde51f5d7511fc368b661726ce936bc0a278366bf3a243f2bc33375af45b437f9f256591a16caa4c46bbe16fee9ad0d188de18686928f34753d'
