d4e8ced0da4636fdc7c5845904bef49ae04045b1
Puru <puruchoudhary@gmail.com>
2011-07-22 00:25:59 -0700

    Moved keys and secrets to yml file

--- lib/twitter_wrapper.rb
+++ lib/twitter_wrapper.rb
@@ -2,20 +2,15 @@ class TwitterWrapper
    attr_reader :tokens
    
    def initialize(config)
-     # File.join(Rails.root, 'config', 'twitter.yml')
      @config = config
      @tokens = YAML::load_file @config
      @callback_url = @tokens['callback_urls'][Rails.env]
-     #puts "CB:#{@callback_url}"
-     @auth = Twitter::OAuth.new('xcQnowmpUKU7KTWjiyg','fuenh1AbUpKSrFTMUvRO4w9tKPE9w5cUgRnf7QNgE')
-     #@auth = OAuth::Consumer.new('xcQnowmpUKU7KTWjiyg', 'fuenh1AbUpKSrFTMUvRO4w9tKPE9w5cUgRnf7QNgE',
-    #                                   {:site => "http://api.twitter.com"})
+     @auth = Twitter::OAuth.new(@tokens['consumer_key'], @tokens['consumer_secret'])
+
    end
    
    def request_tokens
-     puts "CallbackURL:#{@callback_url} Auth:#{@auth}"
      @request_token = @auth.request_token :oauth_callback => @callback_url
-     puts "Tokens:#{@request_token.token} #{@request_token.secret}"
      [@request_token.token, @request_token.secret]
    end
    
@@ -24,9 +19,7 @@ class TwitterWrapper
    end
    
    def auth(rtoken, rsecret, verifier)
-     puts "Auth data:#{rtoken} #{rsecret} #{verifier}"
      @auth.authorize_from_request(rtoken, rsecret, verifier)
-     puts "Access token: #{@auth.access_token.token} Access secret: #{@auth.access_token.secret}"
      [@auth.access_token.token, @auth.access_token.secret]
    end
    
